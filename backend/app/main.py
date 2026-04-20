07d3ae1c33c04d2cd9562fe1bc643ef0e662e39f
IgnacioZDev7 <ignaciozarate.dev@gmail.com>
2026-04-19 21:19:24 -0400

    Modificación en el main para los permisos cors, el sistema funciona normal ejecutando el servidor de backend y frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
 # Inicializar FastAPI
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION
+)
+
+# Permisos CORS para conectar validamente el frontend con el backend en entorno local
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 from app.api.routes import frases
